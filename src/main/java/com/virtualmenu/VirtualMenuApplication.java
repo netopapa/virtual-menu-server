87f7c6d1e36bbff54a7c54ec037482ff80378fd6
papaneto <vspapaneto@gmail.com>
2019-11-03 19:49:33 -0400

    last changes

--- src/main/java/com/virtualmenu/VirtualMenuApplication.java
+++ src/main/java/com/virtualmenu/VirtualMenuApplication.java
@@ -2,12 +2,35 @@ package com.virtualmenu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-@SpringBootApplication
+import java.util.Arrays;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class VirtualMenuApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VirtualMenuApplication.class, args);
     }
+
+    @Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(Arrays.asList("*"));
+		config.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
+				HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name(), HttpMethod.HEAD.name()));
+		config.setAllowedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION, HttpHeaders.CACHE_CONTROL,
+				HttpHeaders.CONTENT_TYPE, "X-CSRF-Token", "X-Requested-With"));
+		config.setAllowCredentials(true);
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 
 }
